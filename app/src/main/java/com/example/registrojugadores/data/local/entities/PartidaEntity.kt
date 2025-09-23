a14abb098decd0f02efba3c150361bf540dc2b49
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-09-23 01:01:11 -0400

    Elimine el signo de ?

--- app/src/main/java/com/example/registrojugadores/data/local/entities/PartidaEntity.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/entities/PartidaEntity.kt
@@ -34,7 +34,7 @@ import java.util.Locale
 
 data class PartidaEntity (
     @PrimaryKey(autoGenerate = true)
-    val partidaId: Int? = 0,
+    val partidaId: Int = 0,
     val fecha: String = "",
     val jugador1Id: Int = 0,
     val jugador2Id: Int = 0,

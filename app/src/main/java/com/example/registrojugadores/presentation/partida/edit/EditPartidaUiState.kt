d61facfc82c3733aee8794fb4dd5d68302a8d445
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-09-23 01:01:59 -0400

    Agregue campos faltantes

--- app/src/main/java/com/example/registrojugadores/presentation/partida/edit/EditPartidaUiState.kt
+++ app/src/main/java/com/example/registrojugadores/presentation/partida/edit/EditPartidaUiState.kt
@@ -1,16 +1,24 @@
 package com.example.registrojugadores.presentation.partida.edit
 
+import com.example.registrojugadores.domain.jugador.model.Jugador
+
 data class EditPartidaUiState (
-    val isNew: Boolean = true,
     val partidaId: Int? = null,
     val fecha: String = "",
     val jugador1Id: Int = 0,
     val jugador2Id: Int = 0,
     val ganadorId: Int = 0,
+    val isNew: Boolean = true,
     val esFinalizada: Boolean = false,
     val jugador1Error: String? = null,
     val jugador2Error: String? = null,
     val isSaving: Boolean = false,
     val isDeleting: Boolean = false,
-    val deleted: Boolean = false
+    val deleted: Boolean = false,
+    val saved: Boolean = false,
+
+    val listaJugadores: List<Jugador> = emptyList(),
+    val jugadoresLoading: Boolean = true
+
+
 )
